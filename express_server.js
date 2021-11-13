33b2b454d5aef5c3529b525308dbfe710e0d06c2
jackiestchen <jackiestchen@gmail.com>
2021-11-12 19:32:44 -0700

    Added new view url_view

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDataBase = {
@@ -26,11 +27,28 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app. get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDataBase[req.params.shortURL]};
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 })
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
+function generateRandomString() {
+  
+}
